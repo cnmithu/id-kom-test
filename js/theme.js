a991ada6d460ef6c7d07af3202a0ef4d092c668f
cnmithu <cnmithu@gmail.com>
2020-09-03 17:05:00 +0200

    Designed Register section and implemented simple custom validation

--- js/theme.js
+++ js/theme.js
@@ -7058,4 +7058,63 @@
     $(this).find('.custom-carousel-indicators li[data-slide-to="' + event.from + '"]').removeClass('active');
     $(this).find('.custom-carousel-indicators li[data-slide-to="' + event.to + '"]').addClass('active');
   });
+  /*
+  Register form custom validation
+  */
+
+  let registerFormValidate = false;
+  $(".register-form").bind('input', function (e) {
+    registerFormValidate = true;
+    $(this).find('input').each(function (index, input) {
+      let inputTpye = $(this).attr('type');
+      let inputValue = $(this).val().trim();
+      let isvalid = false;
+
+      switch (inputTpye) {
+        case 'text':
+          if (inputValue.length) isvalid = true;
+          break;
+
+        case 'email':
+          if (/\S+@\S+\.\S+/.test(inputValue)) isvalid = true;
+          break;
+
+        case 'password':
+          if (inputValue.length) {
+            isvalid = true;
+            let inputName = $(input).attr('name');
+
+            if (inputName == 'cpassword') {
+              let passwordValue = $(e.currentTarget).find('input[name="password"]').val();
+              isvalid = passwordValue.trim() === inputValue;
+            }
+          }
+
+          break;
+
+        case 'checkbox':
+          if ($(this).is(':checked')) isvalid = true;
+
+        default:
+      }
+
+      if (!isvalid) registerFormValidate = isvalid;
+
+      if (e.target == input) {
+        if (isvalid) {
+          $(input).removeClass('is-invalid').addClass('is-valid');
+        } else {
+          $(input).removeClass('is-valid').addClass('is-invalid');
+        }
+      }
+    });
+    $(e.currentTarget).find('button[type="submit"]').prop("disabled", !registerFormValidate);
+  });
+  /*
+  Register form submit handler
+     */
+
+  $(".register-form").submit(function (event) {
+    event.preventDefault();
+  });
 })(jQuery);
--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -63,6 +63,53 @@ get_header();
             </ol>
         </div>
     </section>
+
+    <section class="register bg-secondary py-5 mt-5">
+        <div class="container-fluid pt-5">
+            <div class="row no-gutters-right">
+                <div class="col-12 col-md-7">
+                    <div class="pr-0 pr-xl-4">
+                        <h1>Registrera dig</h1>
+                        <p class="pt-2 text-justify">Etiam convallis elementum sapien, a aliquam turpis aliquam vitae. Pra esent sollici tudin felis vel mi facilisis posuere. Nulla ultrices facilisis ju sto, non varius nisl semp ervel. Interdum et malesuada fames ac antei psum primis in faucibus. Phasellus at ante mattis, condimentum velite t, dignissim nunc. Integer quis tincidunt purus. Duis dignissim maurisv el elit commodo, eu hendrerit leo ultrices. Nulla vehicula vestibulum p urus at rutrum. Pellentesque habitant morbi tristique senectus et netu s et malesuada fames ac turpis egestas. Curabitur dignissim massa ne c libero scelerisque rutrum. Curabitur ac purus id elit hendrerit lacinia.</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-5">  
+                <form id="register-form" action="" class="register-form">
+                    <div class="alert alert-warning alert-dismissible fade d-none" role="alert">
+                        <span class='text'></span>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="name" placeholder="Name*">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" required name="email"  placeholder="Epost*">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" required name="yob"  placeholder="Födelseår*">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" required name="password" placeholder="Lösenord*">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" required name="cpassword" placeholder="Bekräfta Lösenord*">
+                    </div>
+
+                    <div class="form-check d-flex align-items-center">
+                        <input type="checkbox" name='terms' class="form-check-input mr-2">
+                        <label class="form-check-label ">Etiam convallis elementum spien, a aliquam turpis.</label>
+                    </div>
+                    <p class="text-justify terms-condition-text">Etiam convallis elementum sapien, a aliquam turpis aliquam vitae. Praesent sollicit udi n felis vel mi facilisis posuere. Nulla ultrices facilisis justo, non varius nisl semper vel. I nterdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus at ante matti s, condimentum velit et, dignissim nunc. Integer quis tincidunt purus.</p>
+                    <button type="submit" disabled class="btn btn-info btn-block rounded-0 border-0">Skicka</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    
     
 </main>
 
